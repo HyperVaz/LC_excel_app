638ecf0442ae80ab78e13baf4338c1f49bdb90ba
HyperVaz <timvas14@gmail.com>
2025-04-06 13:56:11 +0300

    Добавил навигацию

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,31 +1,21 @@
 <template>
     <div class="w-96 mx-auto">
-        <h1>My App</h1>
+        <Navigation />
         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-            <Link :href="route('dashboard')">
-                Dashboard
-            </Link>
-            <Link :href="route('project.index')">
-                Projects
-            </Link>
-            <Link :href="route('project.import')">
-                Import
-            </Link>
-            <Link :href="route('task.index')">
-                Tasks
-            </Link>
+
         </div>
         <slot></slot>
     </div>
 </template>
 
 <script>
-import {Link} from '@inertiajs/vue3'
+import {Link} from '@inertiajs/vue3';
+import Navigation from "@/Components/Navigation.vue";
 export default {
     name: "MainLayout",
     components:{
         Link,
-
+        Navigation
     }
 }
 </script>
