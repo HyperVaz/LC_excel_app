ab52764958acc5d9a5213f2d9cf12d547fe0bf7b
HyperVaz <timvas14@gmail.com>
2025-04-07 20:00:42 +0300

    Добавил форму отправки

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::get('/', function () {
 
 Route::get('/projects', [\App\Http\Controllers\ProjectController::class, 'index'])->name('project.index');
 Route::get('/projects/import', [\App\Http\Controllers\ProjectController::class, 'import'])->name('project.import');
+Route::post('/projects/import', [\App\Http\Controllers\ProjectController::class, 'importStore'])->name('project.import.store');
 Route::get('/tasks', [\App\Http\Controllers\TaskController::class, 'index'])->name('task.index');
 
 Route::get('/dashboard', function () {
