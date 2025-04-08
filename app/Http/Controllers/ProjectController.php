4583c9e832ab06190f618e8bfff9c2912c5f59a7
HyperVaz <timvas14@gmail.com>
2025-04-08 20:00:37 +0300

    Добавил laravel excel и jobs

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Project\ImportStoreRequest;
+use App\Jobs\ImportProjectExcelFileJob;
 use App\Models\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -22,5 +23,6 @@ class ProjectController extends Controller
     public function importStore(ImportStoreRequest $request){
         $data = $request->validated();
         $path = File::putAndCreate($data['file']);
+        ImportProjectExcelFileJob::dispatchNow($path);
     }
 }
