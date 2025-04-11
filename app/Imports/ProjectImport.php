cea268a9ed69a78d92381a13aa552a9f1ead6951
HyperVaz <timvas14@gmail.com>
2025-04-11 14:58:53 +0300

    Add factory, validation

--- app/Imports/ProjectImport.php
+++ app/Imports/ProjectImport.php
@@ -2,18 +2,94 @@
 
 namespace App\Imports;
 
+use App\Factory\ProjectFactory;
+use App\Models\FailedRow;
+use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Validators\Failure;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class ProjectImport implements ToCollection
+class ProjectImport implements ToCollection, WithHeadingRow, WithValidation, SkipsOnFailure
 {
     /**
-    * @param Collection $collection
-    */
+     * @param Collection $collection
+     */
     public function collection(Collection $collection)
     {
+        $typesMap = $this->getTypesMap(Type::all());
+
         foreach ($collection as $row) {
-            dd($row);
+            if (!isset($row['naimenovanie'])) continue;
+
+            $projectFactory = ProjectFactory::make($typesMap, $row);
+            Project::UpdateOrCreate([
+                'type_id' => $projectFactory->getValues()['type_id'],
+                'title' => $projectFactory->getValues()['title'],
+                'created_at_time' => $projectFactory->getValues()['created_at_time'],
+                'contracted_at' => $projectFactory->getValues()['contracted_at']],
+                $projectFactory->getValues());
         }
+
+    }
+
+    private function getTypesMap($types): array
+    {
+        $map = [];
+        foreach ($types as $type) {
+            $map[$type->title] = $type->id;
+        }
+        return $map;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'tip' => 'required|string',
+            'naimenovanie' => 'required|string',
+            'data_sozdaniia' => 'required|integer',
+            'podpisanie_dogovora' => 'required|integer',
+            'dedlain' => 'nullable|integer',
+            'setevik' => 'nullable|string',
+            'nalicie_autsorsinga' => 'nullable|string',
+            'nalicie_investorov' => 'nullable|string',
+            'sdaca_v_srok' => 'nullable|string',
+            'kolicestvo_uslug' => 'nullable|integer',
+            'kolicestvo_ucastnikov' => 'nullable|integer',
+            'kommentarii' => 'nullable|string',
+            'vlozenie_v_pervyi_etap' => 'nullable|integer',
+            'vlozenie_vo_vtoroi_etap' => 'nullable|integer',
+            'vlozenie_v_tretii_etap' => 'nullable|integer',
+            'vlozenie_v_cetvertyi_etap' => 'nullable|integer',
+            'znacenie_effektivnosti' => 'nullable|numeric',
+
+
+        ];
+    }
+
+    public function onFailure(Failure ...$failures)
+    {
+        $map = [];
+        foreach ($failures as $failure) {
+            foreach ($failure->errors() as $error) {
+                $map[]= [
+                    'key' => $failure->attribute(),
+                    'row' => $failure->row(),
+                    'message' => $error,
+                    'task_id' => 1
+                ];
+            }
+        }
+        if (count($map) > 0) FailedRow::insertFailedRows($map);
+    }
+
+    public function customValidationMessages():array{
+        return [
+            'data_sozdaniia.string' => 'это поле должно быть числом'
+        ];
     }
 }
